4edb02d1f35b905163ef7c95019541bbc00c325b
Joel Kirchartz <JKirchartz@gmail.com>
2020-05-22 14:32:59 -0400

    Add existing code
    I tried to create this repo from https://repl.it/@JKirchartz/UnacceptableWindyLibrary
    
    but there's a bug in repl.it that won't actually push my commits here, even though they appear to made on repl.it

---
+++ main.py
@@ -0,0 +1,10 @@
+import sodoku
+
+# IDEA: use Sodoku puzzles to generate music
+# TODO: learn Kodou https://kodou.readthedocs.io/en/latest/
+# maybe learn mingus instead? http://bspaans.github.io/python-mingus/
+
+puzzle = sodoku.SudokuGenerator()
+
+puzzle.print_grid("full solution")
+
